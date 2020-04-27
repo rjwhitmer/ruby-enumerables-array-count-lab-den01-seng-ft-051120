d61074db26bf6e27c642aad7a5d877cf315a5b6c
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-27 19:02:58 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,25 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  strings = 0 
+  
+  array.count do |element|
+    if element.is_a?(String)
+      strings += 1 
+    end
+  end
+  strings
+  
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  is_empty = 0 
+  
+  array.count do |element|
+    if element == ""
+      is_empty += 1 
+    end
+  end
+  
+  is_empty
 end
